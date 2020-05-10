bcaf28e5eee32c971a8358aa1bc7717a7b151774
enZo <inbox.huytran@gmail.com>
2020-05-10 23:59:02 +0700

    under construction

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default () => (
-    <div>
-         <Link href='/about'>
-            <a>About</a>
-        </Link>
-        <p>Hello Next.js</p>
-    </div>  
+    <div className="container">
+        <Head>
+            <title>Site Maintenance</title>
+        </Head>
+        <main>
+            <article>
+                <h1>We&rsquo;ll be back soon!</h1>
+                <div>
+                    <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:#">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
+                    <p>&mdash; The Team</p>
+                </div>
+            </article>
+        </main>
+        
+        <style jsx global>{`
+        body { text-align: center; padding: 150px; }
+        h1 { font-size: 50px; }
+        body { font: 20px Helvetica, sans-serif; color: #333; }
+        article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+        a { color: #dc8100; text-decoration: none; }
+        a:hover { color: #333; text-decoration: none; }
+      `}</style>
+
+    </div>
+
    )
