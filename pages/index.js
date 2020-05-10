d573699d114e63a24b59723b771f57bce70edf21
enZo <inbox.huytran@gmail.com>
2020-05-11 00:17:12 +0700

    update css to global css

--- pages/index.js
+++ pages/index.js
@@ -15,15 +15,6 @@ export default () => (
                 </div>
             </article>
         </main>
-        
-        <style jsx global>{`
-        body { text-align: center; padding: 150px; }
-        h1 { font-size: 50px; }
-        body { font: 20px Helvetica, sans-serif; color: #333; }
-        article { display: block; text-align: left; width: 650px; margin: 0 auto; }
-        a { color: #dc8100; text-decoration: none; }
-        a:hover { color: #333; text-decoration: none; }
-      `}</style>
 
     </div>
 
