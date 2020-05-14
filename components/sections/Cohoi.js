01b31a3f1457b4f782aeb54ed3722ec4348c4f65
enZo <inbox.huytran@gmail.com>
2020-05-14 20:35:15 +0700

    update data caller

--- components/sections/Cohoi.js
+++ components/sections/Cohoi.js
@@ -9,7 +9,7 @@ const Cohoi = props => (
                     <div className="row align-items-center mt-5">
                         <div className="col-xl-6 pr-0 pr-xl-5 col-md-5 col-sm-11 col-12 mb-5 mb-lg-0">
                             <div className="heading-block nobottomborder mb-4">
-                                <div className="before-heading mb-2">{props.appT[`line1`]} <span className="badge badge-secondary ml-1"> DELIVERY </span></div>
+                                <div className="before-heading mb-2">{props.appT[`line1`]} </div>
                                 <h2 className="t600 ls0 nott">
                                     <span className="underline">{props.appT[`line2`]}</span>
                                     <br /> {props.appT[`line3`]}
@@ -21,14 +21,14 @@ const Cohoi = props => (
                             </p>
                             <p><i className="icon-angle-right1"></i> {props.appT[`line6`]}
                             </p>
-                            <a href="#" className="button button-large nott ml-0"><i className="icon-line-download"></i> {props.appT[`line7`]}</a>
+                            <a href= {props.appT[`btn_href`]} className="button button-large nott ml-0"><i className="icon-line-download"></i> {props.appT[`btn_title`]}</a>
                         </div>
                         <div className="col-xl-3 col-md-3 col-6 parallax" data-0="transform: translateY(0px);" data-600="transform: translateY(-30px);">
                             <img src= { props.appT.img_src1 } alt="" className="rounded" data-animate="faster fadeInLeft" />
                             <div className="card mt-3" data-animate="faster fadeInLeft" data-delay="100">
                                 <div className="card-body rounded bgcolor dark">
-                                    <h4 className="mb-2 ">500 đơn hàng/ngày</h4>
-                                    <p className="mb-0">* Tổng trung bình đơn hàng của các nhà hàng trên ứng dụng giao hàng tại tp HCM</p>
+                                    <h4 className="mb-2 "> {props.appT[`highlight_title`]}</h4>
+                                    <p className="mb-0">{props.appT[`highlight_content`]}</p>
                                 </div>
                             </div>
                         </div>
